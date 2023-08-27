d7bfa5853921f48900b3b87ec955792e27987371
samuel.abreu <samuel.abreu@fleetcor.com.br>
2023-08-27 20:30:28 -0300

    feat: improve the create opening handler

--- handler/createOpening.go
+++ handler/createOpening.go
@@ -7,6 +7,18 @@ import (
 	"github.com/steixeira93/gopportunities/schemas"
 )
 
+// @BasePath /api/v1
+
+// @Summary Create opening
+// @Description Create a new job opening
+// @Tags Openings
+// @Accept json
+// @Produce json
+// @Param request body CreateOpeningRequest true "Request body"
+// @Success 200 {object} CreateOpeningResponse
+// @Failure 400 {object} ErrorResponse
+// @Failure 500 {object} ErrorResponse
+// @Router /opening [post]
 func CreateOpeningHandler(ctx *gin.Context) {
 	request := CreateOpeningRequest{}
 
@@ -19,16 +31,16 @@ func CreateOpeningHandler(ctx *gin.Context) {
 	}
 
 	opening := schemas.Opening{
-		Role: request.Role,
-		Company: request.Company,
+		Role:     request.Role,
+		Company:  request.Company,
 		Location: request.Location,
-		Remote: *request.Remote,
-		Link: request.Link,
-		Salary: request.Salary,
+		Remote:   *request.Remote,
+		Link:     request.Link,
+		Salary:   request.Salary,
 	}
 
 	if err := db.Create(&opening).Error; err != nil {
-		logger.Errorf("Error creating opening: %v", err.Error())
+		logger.Errorf("error creating opening: %v", err.Error())
 		sendError(ctx, http.StatusInternalServerError, "error creating opening on database")
 		return
 	}
