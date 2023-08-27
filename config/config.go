0bc888b6aff21ea5ab942ba8463cf92e87027638
samuel.abreu <samuel.abreu@fleetcor.com.br>
2023-08-27 17:34:32 -0300

    feat: add database handler

--- config/config.go
+++ config/config.go
@@ -1,6 +1,10 @@
 package config
 
-import "gorm.io/gorm"
+import (
+	"fmt"
+
+	"gorm.io/gorm"
+)
 
 var (
 	db *gorm.DB
@@ -8,7 +12,21 @@ var (
 )
 
 func Init() error {
+
+	var err error 
+
+	//Initialize SQLite
+	db, err = InitialzeSqlite()
+
+	if err != nil {
+		return fmt.Errorf("error initializing sqlite: %v", err)
+	}
+
 	return nil
+}
+
+func GetSQLiteDB() *gorm.DB {
+	return db
 }
 
 func GetLogger(p string) *Logger {
