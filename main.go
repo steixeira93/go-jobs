aafbf4812e21e770cc775241d9f8b5673f9f997b
samuel.abreu <samuel.abreu@fleetcor.com.br>
2023-08-27 16:51:00 -0300

    feat: add custom logger

--- main.go
+++ main.go
@@ -1,8 +1,25 @@
 package main
 
-import "github.com/steixeira93/gopportunities/router"
+import (
+	"github.com/steixeira93/gopportunities/config"
+	"github.com/steixeira93/gopportunities/router"
+)
+
+var (
+	logger *config.Logger
+)
 
 func main() {
 
+	logger = config.GetLogger("main")
+
+	// Initialize Congifs
+	err := config.Init()
+	if err != nil {
+		logger.Errorf("Error initializing configs: %v", err)
+		return
+	}
+
+	// Initialize Router
 	router.Initialize()
 }
