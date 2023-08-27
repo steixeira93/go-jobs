483875a3c450c4b3e26c468f9742a4ad6c8de154
samuel.abreu <samuel.abreu@fleetcor.com.br>
2023-08-27 20:13:23 -0300

    feat: add swagger configuration

---
+++ makefile
@@ -0,0 +1,20 @@
+.PHONY: default run build test docs clean
+
+APP_NAME=gopportunities
+
+default: run-with-docs
+
+run: 
+	@go run main.go
+run-with-docs:
+	@swag init
+	@go run main.go
+build: 
+	@go build -o $(APP_NAME) main.go
+test: 
+	@go test ./...
+docs:
+	@swag init
+clean:
+	@rm -rf $(APP_NAME) docs
+	@rm -rf ./docs
--- router/routes.go
+++ router/routes.go
@@ -3,19 +3,26 @@ package router
 import (
 	"github.com/gin-gonic/gin"
 	"github.com/steixeira93/gopportunities/handler"
+	docs "github.com/steixeira93/gopportunities/docs"
+	swaggerfiles "github.com/swaggo/files"
+	ginSwagger "github.com/swaggo/gin-swagger"
 )
 
 func initializeRoutes(router *gin.Engine) {
 
 	//Initialize Handler
 	handler.InitializeHandler()
+	basePath := "/api/v1"
+	docs.SwaggerInfo.BasePath = basePath
 	
-	v1 := router.Group("/api/v1")
+	v1 := router.Group(basePath)
 	{
-		v1.GET("/openings/:id", handler.ShowOpeningHandler)
+		v1.GET("/opening", handler.ShowOpeningHandler)
 		v1.POST("/opening", handler.CreateOpeningHandler)
-		v1.DELETE("/opening/:id", handler.DeleteOpeningHandler)
-		v1.PUT("/opening/:id", handler.UpdateOpeningHandler)
+		v1.DELETE("/opening", handler.DeleteOpeningHandler)
+		v1.PUT("/opening", handler.UpdateOpeningHandler)
 		v1.GET("/openings", handler.ListOpeningsHandler)
 	}
+
+	router.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerfiles.Handler))
 }
